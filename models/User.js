496fa105ece11242e111a49ea5861912cbf509fd
sunniestsunday <itihwesunday@gmail.com>
2021-07-26 03:42:30 -0700

    weektask

---
+++ models/User.js
@@ -0,0 +1,11 @@
+// make shift database
+const userList = [
+    {
+        id : 1,
+        email : "itihwesunday@gmail.com",
+        fullName : "Itihwe  Sunday Avwenayeri",
+        password : "sunnyforlive2121#"
+    }
+]
+module.exports = userList
+
---
+++ models/products.js
@@ -0,0 +1,20 @@
+let productList = [
+    {
+        id: 1,
+        name: "Kettle",
+        description: "used for boiling water",
+        image: "String",
+        price: 60
+
+    },
+    {
+        id: 2,
+        name: "Filter",
+        description: "used for seiving",
+        image: "String",
+        price: 70
+
+    }
+];
+
+module.exports = productList;
